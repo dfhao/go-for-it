a793bdcbe34aaf4aff01b7901acf1c5f4b412960
dfhao <dfhao12@gmail.com>
2021-03-19 00:13:38 +0800

    常规提交

---
+++ go-ispringboot/src/main/java/com/dfhao/ispringboot/test/Person.java
@@ -0,0 +1,7 @@
+package com.dfhao.ispringboot.test;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "person")
+public class Person {
+}
