c9620b7d45686e6ef46514b55a94c380e816ad6b
dfhao <dfhao12@gmail.com>
2021-03-22 21:37:06 +0800

    常规

---
+++ go-ispringboot/src/main/java/com/dfhao/ispringboot/test/bean/User.java
@@ -0,0 +1,84 @@
+package com.dfhao.ispringboot.test.bean;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * 测试程序
+ *
+ * @author :  dfhao
+ * @date :  2021/3/19 10:50
+ */
+@Table("user_t")
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String userName;
+    @Column(nullable = false)
+    private String passWord;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = true, unique = true)
+    private String nickName;
+    @Column(nullable = false)
+    private String regTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord == null ? null : passWord.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public String getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(String regTime) {
+        this.regTime = regTime == null ? null : regTime.trim();
+    }
+}
